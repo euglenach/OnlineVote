65dd0008a9ad3fbb5ec53b103011c0c5523bc737
Euglenach <midorimushiiii@gmail.com>
2021-12-12 22:27:40 +0900

    かんきょーこーちく

---
+++ Server/Server/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MagicOnion.Hosting;
+using Microsoft.Extensions.Hosting;
+
+await MagicOnionHost.CreateDefaultBuilder()
+                    .UseMagicOnion()
+                    .RunConsoleAsync();
